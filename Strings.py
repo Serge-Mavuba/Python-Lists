ea782a8af0214dae764e7dddbb976aa429d08acb
Serge-Mavuba <79016443+Serge-Mavuba@users.noreply.github.com>
2021-03-18 15:17:35 -0400

    Delete Strings.py

--- Strings.py
+++
@@ -1,7 +0,0 @@
-# Strings reprensent text created using single (') double (") or triple (""") quotes
-# Consider the Variable NAME with String Name "Hi what is your name: "
-#      and the Variable MESSAGE with String Name 'how are you?'
-NAME = input("Hi what is your name: ")
-MESSAGE = 'how are you?' 
-print(NAME, MESSAGE) 
-# The output of should read: Hello Serge, how are you?
